8da0e93520fe7a9c1c90bddac5c1a85ab160df3a
kenichi-itou <renpaps4407@gmail.com>
2026-05-31 17:11:06 +0900

    feat: 商品出品機能のテストコード追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_fee_id { 1 }
-    prefecture_id { 1 }
-    shipping_day_id { 1 }
-    price { 1 }
-    user { nil }
+    name { 'テスト商品' }
+    description { 'テスト用の商品です' }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
+    price { 1000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
