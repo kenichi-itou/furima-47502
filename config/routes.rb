23888cd070052de9015f23d561a2293dd9dcaa71
kenichi-itou <renpaps4407@gmail.com>
2026-06-06 14:41:37 +0900

    fix: 削除機能をコメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: %i[index new create show edit update destroy]
+  resources :items, only: %i[index new create show edit update]
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
