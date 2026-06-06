e9b292592b999b5b5ab4921a01fb56a1e4af38d3
kenichi-itou <renpaps4407@gmail.com>
2026-06-07 07:37:52 +0900

    fix: 売却済み商品の編集ページアクセス制限と詳細ページSold Out表示を追加

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,31 +1,29 @@
 document.addEventListener('turbo:load', () => {
   const cardElement = document.getElementById('card-element');
+  if (!cardElement) return;
 
-  if (cardElement) {
-    const publicKey = document.querySelector('meta[name="payjp-public-key"]').content;
-    const payjp = Payjp(publicKey);
-    const elements = payjp.elements();
-    const cardNumberElement = elements.create('cardNumber');
-    const cardExpiryElement = elements.create('cardExpiry');
-    const cardCvcElement = elements.create('cardCvc');
+  const publicKey = document.querySelector('meta[name="payjp-public-key"]').content;
+  const payjp = Payjp(publicKey);
+  const elements = payjp.elements();
+  const cardNumberElement = elements.create('cardNumber');
+  const cardExpiryElement = elements.create('cardExpiry');
+  const cardCvcElement = elements.create('cardCvc');
 
-    cardNumberElement.mount('#card-number');
-    cardExpiryElement.mount('#card-expiry');
-    cardCvcElement.mount('#card-cvc');
+  cardNumberElement.mount('#card-number');
+  cardExpiryElement.mount('#card-expiry');
+  cardCvcElement.mount('#card-cvc');
 
-    const form = document.getElementById('order-form');
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const result = await payjp.createToken(cardNumberElement);
-      console.log(result);
-      if (result.error) {
-        const errorElement = document.getElementById('card-errors');
-        errorElement.textContent = result.error.message;
-      } else {
-        const tokenElement = document.getElementById('card-token');
-        tokenElement.setAttribute('value', result.id);
-        form.submit();
-      }
-    });
-  }
+  const form = document.getElementById('order-form');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const result = await payjp.createToken(cardNumberElement);
+    if (result.error) {
+      const errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      const tokenElement = document.getElementById('card-token');
+      tokenElement.setAttribute('value', result.id);
+      form.submit();
+    }
+  });
 });
