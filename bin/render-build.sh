9577f0f5ac490d5837130a31a45e21886e2d8f92
kenichi-itou <renpaps4407@gmail.com>
2026-06-07 17:41:10 +0900

    fix: render-build.shのDB操作を分割

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop db:create db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:drop
+bundle exec rake db:create
+bundle exec rake db:migrate
